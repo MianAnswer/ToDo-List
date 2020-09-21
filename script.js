81e476e2a9c2be182b5238567f5cf629767a5dba
Mian Answer <miananswer@hotmail.com>
2020-09-20 22:06:21 -0500

    Create a checkbox for todo item to remove the item from list

--- script.js
+++ script.js
@@ -4,10 +4,19 @@ function hello () {
     if (toDoItemNODE.value !== "") {
         const newDiv = document.createElement("div");
         const divText = document.createTextNode(toDoItemNODE.value);
+
+        const inputCheckbox = document.createElement("input");
+        inputCheckbox.setAttribute("type", "checkbox");
+        inputCheckbox.onclick = deleteItem;
         
+        newDiv.appendChild(inputCheckbox);
         newDiv.appendChild(divText);
 
         document.getElementById("list").appendChild(newDiv);
         toDoItemNODE.value = "";
     }
+}
+
+function deleteItem() {
+    this.parentNode.remove();
 }
